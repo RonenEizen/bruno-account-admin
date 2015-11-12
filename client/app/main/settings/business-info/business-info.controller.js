87dd328adf8f10a0ecb47fc932cfcb233666a4b1
Lucas Engel <lucasengel@gmail.com>
2015-11-12 19:46:42 -0200

    Time-table, html and css fixes, pencil behavior

--- client/app/main/settings/business-info/business-info.controller.js
+++ client/app/main/settings/business-info/business-info.controller.js
@@ -2,6 +2,70 @@
 
 angular.module('userAdminApp')
   .controller('BusinessInfoCtrl', function ($scope, User, Auth) {
+    // time table
+    $scope.timeFormat = "ampm";
+    $scope.weekdays = [{
+      name: "Monday",
+      open: true,
+      split: false,
+      start1: "09:00:00",
+      stop1: "18:00:00",
+      start2: "",
+      stop2: ""
+    }, {
+      name: "Tuesday",
+      open: true,
+      split: true,
+      start1: "09:00:00",
+      stop1: "12:00:00",
+      start2: "13:00:00",
+      stop2: "18:00:00"
+    }, {
+      name: "Wednesday",
+      open: true,
+      split: false,
+      start1: "09:00:00",
+      stop1: "12:00:00",
+      start2: "13:00:00",
+      stop2: "18:00:00"
+    }, {
+      name: "Thurday",
+      open: false,
+      split: false,
+      start1: "09:00:00",
+      stop1: "18:00:00",
+      start2: "",
+      stop2: ""
+    }, {
+      name: "Friday",
+      open: false,
+      split: false,
+      start1: "09:00:00",
+      stop1: "18:00:00",
+      start2: "",
+      stop2: ""
+    }, {
+      name: "Saturday",
+      open: false,
+      split: false,
+      start1: "09:00:00",
+      stop1: "18:00:00",
+      start2: "",
+      stop2: ""
+    }, {
+      name: "Sunday",
+      open: false,
+      split: false,
+      start1: "09:00:00",
+      stop1: "18:00:00",
+      start2: "",
+      stop2: ""
+    }];
+
+
+
+
+    // not sure where is this from
     $scope.errors = {};
 
     $scope.changePassword = function(form) {

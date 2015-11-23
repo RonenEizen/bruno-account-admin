569259b671ede36d21ccbaf2040423b7e694a2c0
Lucas Engel <lucasengel@gmail.com>
2015-11-23 19:34:30 -0200

    Delivery functionality

--- client/app/main/settings/delivery/delivery.controller.js
+++ client/app/main/settings/delivery/delivery.controller.js
@@ -4,6 +4,36 @@ angular.module('userAdminApp')
   .controller('DeliveryCtrl', function ($scope) {
     $scope.enabled = true; // sets delivery enable
 
+    $scope.requestsMaster = {};
+    $scope.requests = {
+      edit: false,
+      email: {
+        enabled: true,
+        input: "lucasengel@gmail.com"
+      },
+      phone: {
+        enabled: false,
+        input: ""
+      }
+    };
+
+    $scope.additionalMaster = {};
+    $scope.additional = {
+      edit: false,
+      minimun: {
+        enabled: true,
+        input: 12344
+      },
+      surcharge: {
+        enabled: false,
+        input: ""
+      },
+      instructions: {
+        enabled: false,
+        input: ""
+      }
+    };
+
     // time table
     $scope.timeFormat = "ampm";
     $scope.weekdays = [{
@@ -63,4 +93,38 @@ angular.module('userAdminApp')
       start2: "",
       stop2: ""
     }];
+
+    // saves & reset forms
+    $scope.reset = function(section) {
+      switch (section) {
+        case 'requests':
+          $scope.requests = angular.copy($scope.requestsMaster);
+          $scope.requests.edit = false;
+          break;
+        case 'delivery':
+          $scope.delivery = angular.copy($scope.deliveryMaster);
+          $scope.delivery.edit = false;
+          break;
+        case 'additional':
+          $scope.additional = angular.copy($scope.additionalMaster);
+          $scope.additional.edit = false;
+          break;
+      };
+    };
+    $scope.save = function(section) {
+      switch (section) {
+        case 'requests':
+          $scope.requestsMaster = angular.copy($scope.requests);
+          $scope.requests.edit = false;
+          break;
+        case 'delivery':
+          $scope.deliveryMaster = angular.copy($scope.delivery);
+          $scope.delivery.edit = false;
+          break;
+        case 'additional':
+          $scope.additionalMaster = angular.copy($scope.additional);
+          $scope.additional.edit = false;
+          break;
+      };
+    };
   });

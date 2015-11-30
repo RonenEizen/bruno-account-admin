f47c41d61387a3200d57b60a4bd456d679d6ff14
Lucas Engel <lucasengel@gmail.com>
2015-11-30 10:15:27 -0200

    Reservation functionality + html fixes

--- client/app/main/settings/reservations/reservations.controller.js
+++ client/app/main/settings/reservations/reservations.controller.js
@@ -6,23 +6,44 @@ angular.module('userAdminApp')
     $scope.seatingMaster = {};
     $scope.seating = {
       edit: false,
-      capacity: ""
+      capacity: ''
     };
-
     $scope.tablesMaster = {};
     $scope.tables = {
       edit: false,
-      sets: [{
-        name: "Booth",
-        capacity: 6,
-        quantity: 5,
-        mergeable: false
-      },{
-        name: "Window",
-        capacity: 2,
-        quantity: 2,
-        mergeable: true
-      }]
+      // clear fields
+      clear: function () {
+        $scope.tableName = '';
+        $scope.tableCapacity = '';
+        $scope.tableQuantity = '';
+        $scope.tableMergeable = false;
+      },
+      // adds new entry
+      add: function (table) {
+        $scope.tables.sets.push(table);
+        $scope.tables.clear();
+      },
+      // set editing true/false to hide add new button
+      editing: false,
+      // copies varibles to inputs
+      get: function (name,capacity,quantity,mergeable,index) {
+        // gets params data to $scope
+        $scope.tableName = name;
+        $scope.tableCapacity = capacity;
+        $scope.tableQuantity = quantity;
+        $scope.tableMergeable = mergeable;
+        $scope.tableIndex = index;
+        // enables update icon
+        $scope.tables.editing = true;
+      },
+      // updates entry
+      update: function (table,index) {
+        $scope.tables.sets.splice(index,1,table);
+        $scope.tables.editing = false;
+        $scope.tables.clear();
+      },
+      // fake data
+      sets: [{ name: "Booth", capacity: 6, quantity: 5, mergeable: false },{ name: "Window", capacity: 2, quantity: 2, mergeable: true }]
     };
 
     // saves & reset forms

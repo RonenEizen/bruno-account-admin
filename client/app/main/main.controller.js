660aed0662fd143db733d277fd7fdb35ac28f12c
Lucas Engel <lucasengel@gmail.com>
2015-12-10 22:24:08 -0200

    Collapsable left sidebar with animation and itens moved to controller

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,25 +1,33 @@
 'use strict';
 
 angular.module('userAdminApp')
-	.controller('MainCtrl', function ($scope, $location, Auth) {
-		$scope.isNavRevealed = false;
+  .controller('MainCtrl', function ($scope, $location, Auth) {
+    $scope.navRevealed = true;
 
-		$scope.menu = [{
-			'title': 'Home',
-			'link': '/'
-		}];
+    $scope.menu = [
+      { link: '/', title: 'Overview' },
+      { link: '/orders', title: 'Orders' },
+      { link: '/reservations', title: 'Reservations' },
+      { link: '/statistics', title: 'Statistics' },
+      { link: '/campaigns', title: 'Campaigns' },
+      { link: '/food-menus', title: 'Food Menus' },
+      { link: '/customers', title: 'Customers' },
+      { link: '/marketing-wizard', title: 'Marketing Wizard' },
+      { link: '/site-builder', title: 'Site Builder' },
+      { link: '/settings', title: 'Settings' },
+      { link: '/login', title: 'Log Out' }
+    ];
 
-		$scope.isCollapsed = true;
-		$scope.isLoggedIn = Auth.isLoggedIn;
-		$scope.isAdmin = Auth.isAdmin;
-		$scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    $scope.getCurrentUser = Auth.getCurrentUser;
 
-		$scope.logout = function() {
-			Auth.logout();
-			$location.path('/login');
-		};
+    $scope.logout = function() {
+      Auth.logout();
+      $location.path('/login');
+    };
 
-		$scope.isActive = function(route) {
-			return route === $location.path();
-		};
-	});
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });

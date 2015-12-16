8c089a951cc4e15aa6677473b5b4bfcd8f3a8e7e
ranisio <ranis.eizen@gmail.com>
2015-12-16 13:32:06 -0500

    Changed the logo image Added new fields to Register form Added yelp’s http api

--- client/app/account/register/register.controller.js
+++ client/app/account/register/register.controller.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('userAdminApp')
-  .controller('RegisterCtrl', function ($scope, Auth, $location) {
+  .controller('RegisterCtrl', function ($scope, Auth, $location, $http) {
     $scope.user = {};
     $scope.errors = {};
+
+    $scope.getPlaces = function(val) {
+      $http.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=AIzaSyCgFtT6tgjUhjZxElvkxs35fxpDtWsUsRo")
+    }
 
     $scope.register = function(form) {
       $scope.submitted = true;

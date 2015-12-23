87eac1f9b42e9694cfc145d4069e9953a9a5f7e8
Lucas Engel <lucasengel@gmail.com>
2015-12-23 15:32:25 -0200

    Reservations shell markup

--- client/app/main/reservations/reservations.controller.js
+++ client/app/main/reservations/reservations.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('userAdminApp')
-  .controller('ReservationsCtrl', function ($scope) {
+  .controller('ReservationsCtrl', function ($scope, $interval) {
     $scope.pageTitle = 'Reservations';
+
+    $scope.view = 'day';
+
+    // this should update time
+    $scope.currentDate = Date;
+    $interval(function() {}, 1000);
   });

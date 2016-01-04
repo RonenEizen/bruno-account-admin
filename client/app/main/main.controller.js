2ad5e42e2f1d47185c4fdd3b687494ed23cc1f51
Lucas Engel <lucasengel@gmail.com>
2016-01-04 14:20:52 -0200

    Markup for new left sidebar design

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,20 +4,7 @@ angular.module('userAdminApp')
   .controller('MainCtrl', function ($scope, $location, Auth) {
 
     $scope.navRevealed = true;
-
-    $scope.menu = [
-      { link: '/', title: 'Overview' },
-      { link: '/orders', title: 'Orders' },
-      { link: '/reservations', title: 'Reservations' },
-      { link: '/statistics', title: 'Statistics' },
-      { link: '/campaigns', title: 'Campaigns' },
-      { link: '/food-menus', title: 'Food Menus' },
-      { link: '/customers', title: 'Customers' },
-      { link: '/marketing-wizard', title: 'Marketing Wizard' },
-      { link: '/site-builder', title: 'Site Builder' },
-      { link: '/settings', title: 'Settings' },
-      { link: '/login', title: 'Log Out' }
-    ];
+    $scope.settingsOpen = false;
 
     $scope.isLoggedIn = Auth.isLoggedIn;
     $scope.isAdmin = Auth.isAdmin;

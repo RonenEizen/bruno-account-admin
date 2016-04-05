18248352e8615b1a4f44fe369a3c546ce77e14d4
Lucas Engel <lucasengel@gmail.com>
2016-04-05 18:46:57 -0300

    Routes for Food Menu and Orders modals

--- client/app/main/food-menus/food-menus.js
+++ client/app/main/food-menus/food-menus.js
@@ -1,53 +1,78 @@
 'use strict';
 
 angular.module('accountAdminApp')
-  .config(function ($stateProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('main.food-menus', {
         url: '/food-menus',
         templateUrl: 'app/main/food-menus/food-menus.html',
         controller: 'FoodMenusCtrl'
       })
-      .state('main.food-menus.viewMenu', {
-        url: '/menu',
-        templateUrl: 'app/main/food-menus/viewFoodMenuModal.html',
-        controller: 'FoodMenusCtrl',
-        controllerAs: 'vm'
-      })
+
       .state('main.food-menus.createMenu', {
-        url: '/createMenu',
-        templateUrl: 'app/main/food-menus/createFoodMenuModal.html',
-        controller: 'FoodMenusCtrl',
-        controllerAs: 'vm'
+        url: '/menu/create',
+        templateUrl: 'app/main/food-menus/createFoodMenuModal.html'
       })
-      .state('main.food-menus.viewCategory', {
-        url: '/viewCategory',
-        templateUrl: 'app/main/food-menus/viewMenuCategoryModal.html',
-        controller: 'FoodMenusCtrl',
-        controllerAs: 'vm'
+      .state('main.food-menus.menu', {
+        abstract: true,
+        url: '/menu/:_id',
+        templateUrl: 'app/main/food-menus/foodMenuModal.html'
       })
+      .state('main.food-menus.menu.edit', {
+        url: '/edit',
+        templateUrl: 'app/main/food-menus/editFoodMenuModal.html'
+      })
+      .state('main.food-menus.menu.remove', {
+        url: '/remove',
+        templateUrl: 'app/main/food-menus/removeFoodMenuModal.html'
+      })
+      .state('main.food-menus.menu.addFood', {
+        url: '/addFood',
+        templateUrl: 'app/main/food-menus/addExistingFoodModal.html'
+      })
+
       .state('main.food-menus.createCategory', {
-        url: '/createCategory',
-        templateUrl: 'app/main/food-menus/createMenuCategoryModal.html',
-        controller: 'FoodMenusCtrl',
-        controllerAs: 'vm'
+        url: '/category/create',
+        templateUrl: 'app/main/food-menus/createMenuCategoryModal.html'
       })
-      .state('main.food-menus.viewFood', {
-        url: '/viewFood',
-        templateUrl: 'app/main/food-menus/viewFoodItemModal.html',
-        controller: 'FoodMenusCtrl',
-        controllerAs: 'vm'
+      .state('main.food-menus.category', {
+        abstract: true,
+        url: '/category/:_id',
+        templateUrl: 'app/main/food-menus/menuCategoryModal.html'
       })
+      .state('main.food-menus.category.edit', {
+        url: '/edit',
+        templateUrl: 'app/main/food-menus/editMenuCategoryModal.html'
+      })
+      .state('main.food-menus.category.remove', {
+        url: '/remove',
+        templateUrl: 'app/main/food-menus/removeMenuCategoryModal.html'
+      })
+
       .state('main.food-menus.createFood', {
-        url: '/createFood',
-        templateUrl: 'app/main/food-menus/createFoodItemModal.html',
-        controller: 'FoodMenusCtrl',
-        controllerAs: 'vm'
+        url: '/food/create',
+        templateUrl: 'app/main/food-menus/createFoodItemModal.html'
       })
-      .state('main.food-menus.existingFood', {
-        url: '/existingFood',
-        templateUrl: 'app/main/food-menus/createExistingFoodModal.html',
-        controller: 'FoodMenusCtrl',
-        controllerAs: 'vm'
+      .state('main.food-menus.food', {
+        abstract: true,
+        url: '/food/:_id',
+        templateUrl: 'app/main/food-menus/foodItemModal.html'
       })
+      .state('main.food-menus.food.view', {
+        url: '/view',
+        templateUrl: 'app/main/food-menus/viewFoodItemModal.html'
+      })
+      .state('main.food-menus.food.edit', {
+        url: '/edit',
+        templateUrl: 'app/main/food-menus/editFoodItemModal.html'
+      })
+      .state('main.food-menus.food.remove', {
+        url: '/remove',
+        templateUrl: 'app/main/food-menus/removeFoodItemModal.html'
+      })
+
+    $urlRouterProvider
+      .when('/food-menus/menu/:_id', '/food-menus/menu/:_id/edit')
+      .when('/food-menus/category/:_id', '/food-menus/category/:_id/edit')
+      .when('/food-menus/food/:_id', '/food-menus/food/:_id/view')
   });
--- client/app/main/orders/orders.js
+++ client/app/main/orders/orders.js
@@ -1,23 +1,30 @@
 'use strict';
 
 angular.module('accountAdminApp')
-  .config(function ($stateProvider) {
+  .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('main.orders', {
         url: '/orders',
         templateUrl: 'app/main/orders/orders.html',
         controller: 'OrdersCtrl'
       })
-      .state('main.orders.view', {
-        url: '/view',
-        templateUrl: 'app/main/orders/viewOrderModal.html',
-        controller: 'OrdersCtrl',
-        controllerAs: 'vm'
-      })
       .state('main.orders.create', {
         url: '/create',
-        templateUrl: 'app/main/orders/createOrderModal.html',
-        controller: 'OrdersCtrl',
-        controllerAs: 'vm'
+        templateUrl: 'app/main/orders/createOrderModal.html'
       })
+      .state('main.orders.order', {
+        abstract: true,
+        url: '/:_id',
+        templateUrl: 'app/main/orders/orderModal.html'
+      })
+      .state('main.orders.order.view', {
+        url: '/view',
+        templateUrl: 'app/main/orders/viewOrderModal.html'
+      })
+      .state('main.orders.order.edit', {
+        url: '/edit',
+        templateUrl: 'app/main/orders/editOrderModal.html'
+      })
+    $urlRouterProvider
+      .when('/orders/:_id', '/orders/:_id/view')
   });
